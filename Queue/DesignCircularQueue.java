e0fd29b26fb0226a343c47a7556e43d3ce46df76
Arif <arifmca467@gmail.com>
2026-09-18 15:11:46 +0530

    Design Circular Queue

---
+++ Queue/DesignCircularQueue.java
@@ -0,0 +1,26 @@
+public class DesignCircularQueue {
+    class Queue {
+        int[] arr;
+        int front;
+        int rear;
+        int size;
+        int capacity;
+
+        Queue(int k) {
+            capacity = k;
+            arr = new int[k];
+            front = 0;
+            size = 0;
+            capacity = 0;
+        }
+
+        public boolean enQueue(int val) {
+            if()
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
